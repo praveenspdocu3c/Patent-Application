abdf5b47ba1e53f582d87a77dc825e453c14543e
praveenspdocu3c <praveens@docu3c.com>
2024-11-13 21:45:50 +0530

    Update hideElements.js

--- hideElements.js
+++ hideElements.js
@@ -32,3 +32,11 @@
 //         specificContainer.style.display = 'none';  
 //     }  
 // });  
+
+
+document.addEventListener("DOMContentLoaded", function() {  
+    const profileContainers = document.querySelectorAll("div._profileContainer_51w34_53, div._profilePreview_51w34_63");  
+    profileContainers.forEach(container => {  
+        container.style.display = 'none';  
+    });  
+});  
