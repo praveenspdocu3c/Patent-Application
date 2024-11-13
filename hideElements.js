3072da67e9f2b2e4bf5a43c9f6d4c5ba531667d3
praveenspdocu3c <praveens@docu3c.com>
2024-11-13 19:32:04 +0530

    Update hideElements.js

--- hideElements.js
+++ hideElements.js
@@ -1,6 +1,19 @@
+// document.addEventListener("DOMContentLoaded", function() {  
+//     const profileContainers = document.querySelectorAll("div._profileContainer_51w34_53, div._profilePreview_51w34_63");  
+//     profileContainers.forEach(container => {  
+//         container.remove();  
+//     });  
+// });  
+
 document.addEventListener("DOMContentLoaded", function() {  
-    const profileContainers = document.querySelectorAll("div._profileContainer_51w34_53, div._profilePreview_51w34_63");  
-    profileContainers.forEach(container => {  
-        container.remove();  
-    });  
+    const iframe = document.querySelector('iframe');  
+    if (iframe) {  
+        iframe.addEventListener('load', function() {  
+            const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;  
+            const profileContainers = iframeDoc.querySelectorAll("div._profileContainer_51w34_53, div._profilePreview_51w34_63");  
+            profileContainers.forEach(container => {  
+                container.style.display = 'none';  
+            });  
+        });  
+    }  
 });  
