95105c68ac523819048bd6eca50846c2a2a3d25e
praveenspdocu3c <praveens@docu3c.com>
2024-11-13 19:36:05 +0530

    Update hideElements.js

--- hideElements.js
+++ hideElements.js
@@ -5,15 +5,23 @@
 //     });  
 // });  
 
+// document.addEventListener("DOMContentLoaded", function() {  
+//     const iframe = document.querySelector('iframe');  
+//     if (iframe) {  
+//         iframe.addEventListener('load', function() {  
+//             const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;  
+//             const profileContainers = iframeDoc.querySelectorAll("div._profileContainer_51w34_53, div._profilePreview_51w34_63");  
+//             profileContainers.forEach(container => {  
+//                 container.style.display = 'none';  
+//             });  
+//         });  
+//     }  
+// });  
+
+
 document.addEventListener("DOMContentLoaded", function() {  
-    const iframe = document.querySelector('iframe');  
-    if (iframe) {  
-        iframe.addEventListener('load', function() {  
-            const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;  
-            const profileContainers = iframeDoc.querySelectorAll("div._profileContainer_51w34_53, div._profilePreview_51w34_63");  
-            profileContainers.forEach(container => {  
-                container.style.display = 'none';  
-            });  
-        });  
-    }  
+    const profileContainers = document.querySelectorAll("div._profileContainer_51w34_53, div._profilePreview_51w34_63");  
+    profileContainers.forEach(container => {  
+        container.style.display = 'none';  
+    });  
 });  
